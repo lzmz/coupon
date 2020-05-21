3512e5dd619a7edb2002e000efa7de885c2afc5e
Leonel Menendez <leonelemenendez@gmail.com>
2020-05-21 00:32:12 -0300

    Initial commit

---
+++ src/test/java/io/github/lzmz/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lzmz.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
