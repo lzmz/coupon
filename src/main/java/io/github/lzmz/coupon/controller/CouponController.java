b71cf0eb11ef72244463c61380c17914a403006f
Leonel Menendez <leonelemenendez@gmail.com>
2020-05-27 22:33:04 -0300

    Refactoring REST responses

--- src/main/java/io/github/lzmz/coupon/controller/CouponController.java
+++ src/main/java/io/github/lzmz/coupon/controller/CouponController.java
@@ -11,10 +11,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -49,10 +49,11 @@ public class CouponController {
             "does not exceed the amount supplied. Additionally, it includes the total amount associated " +
             "to the items of the solution.")
     @PostMapping()
-    public ResponseEntity<CouponSolutionDto> calculate(@Valid @RequestBody CouponCalculateDto couponCalculateDto) throws InsufficientAmountException, NoItemPriceException {
+    @ResponseStatus(HttpStatus.OK)
+    public CouponSolutionDto calculate(@Valid @RequestBody CouponCalculateDto couponCalculateDto) throws InsufficientAmountException, NoItemPriceException {
         Map<String, Float> items = itemConsumerService.getItemsPrice(couponCalculateDto.getItemsId());
         List<String> calculated = couponService.calculate(items, couponCalculateDto.getAmount());
         Float total = couponService.calculateTotalAmount(calculated, items);
-        return new ResponseEntity<>(new CouponSolutionDto(calculated, total), HttpStatus.OK);
+        return new CouponSolutionDto(calculated, total);
     }
 }
--- src/main/java/io/github/lzmz/coupon/exception/RestExceptionHandler.java
+++ src/main/java/io/github/lzmz/coupon/exception/RestExceptionHandler.java
@@ -3,17 +3,15 @@ package io.github.lzmz.coupon.exception;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.lzmz.coupon.dto.response.ConsumerServiceErrorDto;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -26,7 +24,7 @@ import java.util.stream.Collectors;
 /**
  * Class to handle errors appropriately to be sent as responses.
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler {
 
     private final ObjectMapper objectMapper;
@@ -36,95 +34,67 @@ public class RestExceptionHandler {
     }
 
     /**
-     * Retrieves a new API error response entity.
-     *
-     * @param code    the internal code.
-     * @param status  the HTTP status.
-     * @param message the error message.
-     * @param errors  a list of detailed errors.
-     * @return a new {@link ApiError}.
-     */
-    public ResponseEntity<ApiError> getErrorResponse(int code, HttpStatus status, String message, List<String> errors) {
-        ApiError apiError = new ApiError(code, status, message.trim(), errors);
-        return new ResponseEntity<>(apiError, new HttpHeaders(), status);
-    }
-
-    /**
-     * Retrieves a new API error response entity.
-     *
-     * @param code    the internal code.
-     * @param status  the HTTP status.
-     * @param message the error message.
-     * @param error   the detailed errors.
-     * @return a new {@link ApiError}.
-     */
-    public ResponseEntity<ApiError> getErrorResponse(int code, HttpStatus status, String message, String error) {
-        ApiError apiError = new ApiError(code, status, message.trim(), error.trim());
-        return new ResponseEntity<>(apiError, new HttpHeaders(), status);
-    }
-
-    /**
      * Triggered when the requested resource couldn't be found.
      *
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ApiError> handleNoHandlerFoundException() {
+    public ApiError handleNoHandlerFoundException() {
         int code = ApiErrorCode.NOT_FOUND;
-        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        HttpStatus status = HttpStatus.NOT_FOUND;
         String message = "404 error";
         String error = "Requested resource couldn't be found";
-        return getErrorResponse(code, httpStatus, message, error);
+        return new ApiError(code, status, message, error);
     }
 
     /**
      * Triggered when an unsupported media type for a given end point was received.
      *
      * @param ex the {@link HttpMediaTypeNotSupportedException} to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<ApiError> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex) {
+    public ApiError handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex) {
         int code = ApiErrorCode.UNSUPPORTED_MEDIA_TYPE;
-        HttpStatus httpStatus = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+        HttpStatus status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
         String message = "Unsupported media type";
         StringBuilder error = new StringBuilder();
         error.append(ex.getContentType()).append(" media type is not supported. Supported media types are ");
         ex.getSupportedMediaTypes().forEach(mediaType -> error.append(mediaType).append(", "));
-        return getErrorResponse(code, httpStatus, message, error.toString());
+        return new ApiError(code, status, message, error.toString().trim());
     }
 
     /**
      * Triggered when an unsupported HTTP method for a given end point was received.
      *
      * @param ex the {@link HttpRequestMethodNotSupportedException} to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ApiError> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+    public ApiError handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
         int code = ApiErrorCode.METHOD_NOT_ALLOWED;
-        HttpStatus httpStatus = HttpStatus.METHOD_NOT_ALLOWED;
+        HttpStatus status = HttpStatus.METHOD_NOT_ALLOWED;
         String message = "Unsupported HTTP method";
         StringBuilder error = new StringBuilder();
-        error.append(ex.getMethod()).append(" method is not supported for this request. Supported methods are ");
+        error.append(ex.getMethod()).append(" method is not supported for this endpoint. Supported methods are ");
         Objects.requireNonNull(ex.getSupportedHttpMethods()).forEach(method -> error.append(method).append(" "));
-        return getErrorResponse(code, httpStatus, message, error.toString());
+        return new ApiError(code, status, message, error.toString().trim());
     }
 
     /**
      * Triggered when an argument annotated with {@code @Valid} failed on being validated.
      *
      * @param ex the {@link MethodArgumentNotValidException} to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+    public ApiError handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         int code = ApiErrorCode.METHOD_ARGUMENT_NOT_VALID;
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StringBuilder message = new StringBuilder("Invalid arguments: ");
         List<String> errors = new ArrayList<>();
 
@@ -133,86 +103,86 @@ public class RestExceptionHandler {
             message.append(error.getField()).append(" ");
         }
 
-        return getErrorResponse(code, httpStatus, message.toString(), errors);
+        return new ApiError(code, status, message.toString().trim(), errors);
     }
 
     /**
      * Triggered when a fatal problem occurred with content mapping.
      *
      * @param ex the {@link HttpMessageNotReadableException} to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ApiError> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+    public ApiError handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
         int code = ApiErrorCode.MESSAGE_NOT_READABLE;
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         String message = "Invalid body";
         String error = ((JsonProcessingException) ex.getCause()).getOriginalMessage();
-        return getErrorResponse(code, httpStatus, message, error);
+        return new ApiError(code, status, message, error);
     }
 
     /**
      * Triggered when an external service call failed.
      *
      * @param ex the {@link WebClientResponseException} to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.BAD_GATEWAY)
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
     @ExceptionHandler(WebClientResponseException.class)
-    public ResponseEntity<ApiError> handleWebClientResponseException(WebClientResponseException ex) throws JsonProcessingException {
+    public ApiError handleWebClientResponseException(WebClientResponseException ex) throws JsonProcessingException {
         int code = ApiErrorCode.WEB_CLIENT_RESPONSE;
-        HttpStatus httpStatus = HttpStatus.BAD_GATEWAY;
+        HttpStatus status = HttpStatus.BAD_GATEWAY;
         String message = "Error occurred on external call";
         String body = ex.getResponseBodyAsString();
         String error = objectMapper.readValue(body, ConsumerServiceErrorDto.class).getMessage();
-        return getErrorResponse(code, httpStatus, message, error);
+        return new ApiError(code, status, message, error);
     }
 
     /**
      * Triggered when one or more items of a given list of IDs has no associated price.
      *
      * @param ex the exception to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({NoItemPriceException.class})
-    public ResponseEntity<ApiError> handleNoItemPrice(NoItemPriceException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NoItemPriceException.class)
+    public ApiError handleNoItemPrice(NoItemPriceException ex) {
         int code = ApiErrorCode.NO_ITEM_PRICE;
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         String message = "No item price";
         List<String> errors = ex.getIds().stream().map(id -> id + " has no price").collect(Collectors.toList());
-        return getErrorResponse(code, httpStatus, message, errors);
+        return new ApiError(code, status, message, errors);
     }
 
     /**
      * Triggered when a given amount was not enough to buy at least one item.
      *
      * @param ex the exception to handle.
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({InsufficientAmountException.class})
-    public ResponseEntity<ApiError> handleInsufficientAmount(InsufficientAmountException ex) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(InsufficientAmountException.class)
+    public ApiError handleInsufficientAmount(InsufficientAmountException ex) {
         int code = ApiErrorCode.INSUFFICIENT_AMOUNT;
-        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        HttpStatus status = HttpStatus.NOT_FOUND;
         String message = "Insufficient amount";
         String error = "The amount " + ex.getAmount() + " is not enough to buy at least one item";
-        return getErrorResponse(code, httpStatus, message, error);
+        return new ApiError(code, status, message, error);
     }
 
     /**
      * Default Handler. It deals with all other exceptions that don't have specific handlers.
      *
-     * @return a {@link ResponseEntity} object with the error handled.
+     * @return a {@link ApiError} object with the error handled.
      */
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<ApiError> handleAll() {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ApiError handleAll() {
         int code = ApiErrorCode.INTERNAL_SERVER_ERROR;
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         String message = "It's not you. It's us. We are having some problems";
         String error = "Error occurred";
-        return getErrorResponse(code, httpStatus, message, error);
+        return new ApiError(code, status, message, error);
     }
 }
